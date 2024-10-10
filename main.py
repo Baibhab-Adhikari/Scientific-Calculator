5d29064fae5dec3cf29db82df6f235a27e21cd7a
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-10-10 23:12:20 +0530

    refactored code, fixed some menu print statements

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ Usage:
 """
 
 import calc_class as calci
+
 # global bool flag
 calculation = True
 
 
-def main():
+def main() -> None:
     """
-    
+
 
     This function initializes the calculator, displays the main menu, and handles user input
     to perform various calculations. It supports changing the calculator mode, performing
@@ -36,7 +37,7 @@ def main():
     """
     global calculation
     calculator = calci.Calculator()
-    while (calculation):
+    while calculation:
         # instantiate new calculator and print the user menu and reset current value
         print(f"Calculator Mode: {calculator.mode}")
         calci.print_menu()
@@ -52,8 +53,8 @@ def main():
 
         # change calculator mode
         if choice == 6:
-            mode = 'X'
-            while mode not in ['D', 'R']:
+            mode = "X"
+            while mode not in ["D", "R"]:
                 mode = input("Enter D or R to change mode: ").upper()
                 calculator.change_mode(mode)
 
@@ -63,71 +64,70 @@ def main():
             arithmetic_calci = calci.Arithmetic()
             # print user menu for arithmetic
             calci.print_arithmetic_menu()
-            operation = 'wrong'
-            while operation not in ['A', 'B', 'C', 'D']:
+            operation = "wrong"
+            while operation not in ["A", "B", "C", "D"]:
                 operation = input(
-                    "Choose any of the above, (enter a,b,c, or d): ").upper()
+                    "Choose any of the above, (enter a,b,c, or d): "
+                ).upper()
             # addition
-            if operation == 'A':
+            if operation == "A":
                 a, b = calci.input_two_operands()
                 calculator.current_val = arithmetic_calci.addition(a, b)
                 print(f"Sum of {a} and {b} is {calculator.current_val}")
             # subtraction
-            elif operation == 'B':
+            elif operation == "B":
                 a, b = calci.input_two_operands()
                 calculator.current_val = arithmetic_calci.subtraction(a, b)
-                print(f"Difference between {a} and {
-                      b} is {calculator.current_val}")
+                print(f"Difference between {a} and ",
+                      f"{b} is {calculator.current_val}")
             # multiplication
-            elif operation == 'C':
+            elif operation == "C":
                 a, b = calci.input_two_operands()
                 calculator.current_val = arithmetic_calci.multiplication(a, b)
                 print(f"Product of {a} and {b} is {calculator.current_val}")
             # division
-            elif operation == 'D':
+            elif operation == "D":
                 a, b = calci.input_two_operands()
                 calculator.current_val = arithmetic_calci.division(a, b)
                 print(f"{a} divided by {b} is {calculator.current_val}")
             else:
                 pass
             # ask user for continuation
-            # calculation = calci.continue_calculator()
 
         # trigonometry operations
         elif choice == 2:
             trig_calci = calci.Trigonometry()
             calci.print_trigonometry_menu()
-            operation = 'wrong'
-            while operation not in ['A', 'B', 'C', 'D', 'E', 'F']:
+            operation = "wrong"
+            while operation not in ["A", "B", "C", "D", "E", "F"]:
                 operation = input(
-                    "Choose any of the above, (enter a,b,c,d,e, or f): ").upper()
-            if operation == 'A':
+                    "Choose any of the above, (enter a,b,c, d, e, or f): "
+                ).upper()
+            if operation == "A":
                 a = float(input("Enter the angle: "))
                 result = trig_calci.sine(a, calculator.mode)
                 print(f"Sine of {a} is {result}")
-            elif operation == 'B':
+            elif operation == "B":
                 a = float(input("Enter the angle: "))
                 result = trig_calci.cosine(a, calculator.mode)
                 print(f"Cosine of {a} is {result}")
-            elif operation == 'C':
+            elif operation == "C":
                 a = float(input("Enter the angle: "))
                 result = trig_calci.tangent(a, calculator.mode)
                 print(f"Tangent of {a} is {result}")
-            elif operation == 'D':
+            elif operation == "D":
                 a = float(input("Enter the value: "))
                 print(f"Current mode: {calculator.mode}")
                 result = trig_calci.arcsine(a, calculator.mode)
                 print(f"Arcsine of {a} is {result}")
-            elif operation == 'E':
+            elif operation == "E":
                 a = float(input("Enter the value: "))
                 result = trig_calci.arccosine(a, calculator.mode)
                 print(f"Arccosine of {a} is {result}")
-            elif operation == 'F':
+            elif operation == "F":
                 a = float(input("Enter the value: "))
                 result = trig_calci.arctangent(a, calculator.mode)
                 print(f"Arctangent of {a} is {result}")
-            else:
-                pass
 
         # logarithm operations
         elif choice == 3:
@@ -141,7 +141,7 @@ def main():
                 calculator.current_val = log_calci.log(x, calci.E)
                 print(f"log of {x} to base 'e' = {calculator.current_val}")
             else:
-                base = float(base)
+                base = float(base)  # type: ignore
                 calculator.current_val = log_calci.log(x, base)
                 print(f"log of {x} to base {base} = {calculator.current_val}")
 
@@ -151,24 +151,23 @@ def main():
             power_calci = calci.Power()
             # print power class menu
             calci.print_power_menu()
-            operation = 'wrong'
-            while operation not in ['A', 'B', 'C']:
+            operation = "wrong"
+            while operation not in ["A", "B", "C"]:
                 operation = input(
-                    "Choose any of the above, (enter a,b, or c): ").upper()
-            if operation == 'A':
+                    "Choose any of the above, (enter a,b, or c): "
+                ).upper()
+            if operation == "A":
                 b, p = calci.power_input()
                 calculator.current_val = power_calci.exponent(b, p)
                 print(f"{b} raised to {p} = {calculator.current_val}")
-            elif operation == 'B':
+            elif operation == "B":
                 x = float(input("Enter number: "))
                 calculator.current_val = power_calci.root(x)
                 print(f"Square root of {x} = {calculator.current_val}")
-            elif operation == 'C':
+            elif operation == "C":
                 x = float(input("Enter number: "))
                 calculator.current_val = power_calci.cube_root(x)
                 print(f"Cube root of {x} = {calculator.current_val}")
-            else:
-                pass
 
         # complex operations
         elif choice == 5:
@@ -176,24 +175,23 @@ def main():
             complex_calci = calci.Complex()
             # print complex class menu
             calci.print_complex_menu()
-            operation = 'wrong'
-            while operation not in ['A', 'B']:
+            operation = "wrong"
+            while operation not in ["A", "B"]:
                 operation = input(
                     "Choose any of the above, (enter a or b): ").upper()
 
-                if operation == 'A':
+                if operation == "A":
                     z: complex = complex(
-                        input("Enter complex number in rectangular form: "))
+                        input("Enter complex number in rectangular form: ")
+                    )
                     calculator.current_val = complex_calci.to_polar(z)
                     print(f"Polar form of {z} = {calculator.current_val}")
 
-                elif operation == 'B':
+                elif operation == "B":
                     r, phi = calci.complex_input()
                     calculator.current_val = complex_calci.to_rect(r, phi)
-                    print(f"Rectangular form of {r},{
-                          phi} = {calculator.current_val}")
-                else:
-                    pass
+                    print(f"Rectangular form of {r}",
+                          f"{phi} = {calculator.current_val}")
 
         # exiting the calculator app
         elif choice == 7:
